7bcca9e1af28f21f9f43caea020e59ab5342a3b1
talha atif <talhaatif573@gmail.com>
2025-03-22 00:57:23 +0500

    Added login, signup, activity for booking events and seats, added font, added drawables, added github external dependencies for image slider and circular image

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ android {
 dependencies {
 
     implementation(libs.appcompat)
+    implementation ("com.github.denzcoskun:ImageSlideshow:0.1.2")
     implementation ("de.hdodenhof:circleimageview:3.1.0")
     implementation ("com.google.android.material:material:1.11.0")
     implementation(libs.material)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,7 +15,11 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
+        // Maven Central repository
         mavenCentral()
+
+        // JitPack repository (for GitHub-based libraries)
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
