6ff406bb525532bda848256b0da43d6a4fe93b60
talha atif <talhaatif573@gmail.com>
2025-04-02 00:32:47 +0500

    Filter Screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -100,7 +100,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     implementation("androidx.compose.material3:material3")
 
-    implementation ("com.google.android.material:material:1.11.0")
+
 
 
     // Testing
